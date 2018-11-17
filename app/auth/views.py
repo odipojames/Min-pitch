0cc889db34dba267134bf4fd943cc85fb52ba0b9
odipojames <odipojames12@gmai.com>
2018-11-17 09:10:55 +0300

    heroku deployment

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,7 +27,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to One Min Pitch","email/welcome_user",user.email,user=user)
 
         title = "New Account"
         return redirect(url_for('auth.login'))
